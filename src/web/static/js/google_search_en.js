aa1ded4e926a4968967c093fa999424c8193b311
Khourn Khemry <Khemry@khemry.local>
2016-06-23 17:39:13 +0900

    [test] search box.

---
+++ src/web/static/js/google_search_en.js
@@ -0,0 +1,25 @@
+  // (function() {
+  //   //var cx = '014582181594788337797:3sewfx49ixc';
+  //   var cx = '007288577541380389757:zvd2mhxh88y';
+  //   var gcse = document.createElement('script');
+  //   gcse.type = 'text/javascript';
+  //   gcse.async = true;
+  //   gcse.src = (document.location.protocol == 'https:' ? 'https:' : 'http:') +
+  //       '//www.google.com/cse/cse.js?cx=' + cx;
+  //   var s = document.getElementsByTagName('script')[0];
+  //   s.parentNode.insertBefore(gcse, s);
+  // })();
+
+
+
+
+(function() {
+    var cx = '007288577541380389757:zvd2mhxh88y';
+    var gcse = document.createElement('script');
+    gcse.type = 'text/javascript';
+    gcse.async = true;
+    gcse.src = 'https://cse.google.com/cse.js?cx=' + cx;
+    //gcse.src = (document.location.protocol == 'https:' ? 'https:' : 'http:') + '//www.google.com/cse/cse.js?cx=' + cx;
+    var s = document.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(gcse, s);
+})();
